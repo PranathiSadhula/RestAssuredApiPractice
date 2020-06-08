27756bf88ac291dc730f8e6aad78fe084de07d2d
pranathi sadhula <pranathisadhula@gmail.com>
2020-06-08 16:03:51 +0530

    Best buy tasks with RestAssured

---
+++ 60DayApiRestAssured/src/main/java/week1/day2/bestbuy/CanonProductsInGivenPriceRange.java
@@ -0,0 +1,37 @@
+package week1.day2.bestbuy;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class CanonProductsInGivenPriceRange {
+	
+	@Test
+	public void productAvailabilityPostalCodeWithAreaFun() {
+		// Step1 : Base url setup
+				RestAssured.baseURI = "https://api.bestbuy.com/v1";
+				
+				// step2 : Authentication
+
+				// step3 : build params
+				Map<String, String> pMap = new HashMap<String, String>();
+				pMap.put("apiKey", "qUh3qMK14GdwAs9bO59QRSCJ");
+				pMap.put("format", "json");
+				pMap.put("show","sku,name,salePrice");
+				
+				Response resp = RestAssured.given().params(pMap).get("products(manufacturer=canon&salePrice>1000&salePrice<1500)");
+
+				JsonPath jsonresp = resp.jsonPath();
+				jsonresp.prettyPrint();
+				
+				assertTrue(resp.getStatusCode() == 200, "Expected status code : 200 and actual :"+resp.getStatusCode());
+	}
+
+}
---
+++ 60DayApiRestAssured/src/main/java/week1/day2/bestbuy/StorePickUpTrue.java
@@ -0,0 +1,37 @@
+package week1.day2.bestbuy;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class StorePickUpTrue {
+	
+	@Test
+	public void productAvailabilityPostalCodeWithAreaFun() {
+		// Step1 : Base url setup
+				RestAssured.baseURI = "https://api.bestbuy.com/v1";
+				
+				// step2 : Authentication
+
+				// step3 : build params
+				Map<String, String> pMap = new HashMap<String, String>();
+				pMap.put("apiKey", "qUh3qMK14GdwAs9bO59QRSCJ");
+				pMap.put("format", "json");
+				pMap.put("show","name,stores.storeId,stores.name");
+				
+				Response resp = RestAssured.given().params(pMap).get("products(manufacturer=apple&search=iPhone 11 Pro 64GB&inStorePickup=true)+stores(region=RI)");
+
+				JsonPath jsonresp = resp.jsonPath();
+				jsonresp.prettyPrint();
+				
+				assertTrue(resp.getStatusCode() == 200, "Expected status code : 200 and actual :"+resp.getStatusCode());
+	}
+
+}
